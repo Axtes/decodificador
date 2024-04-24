4a612ea25edf31c8214428f76e6a662e3ee107da
david santos <david.sobral.profissional645@gmail.com>
2024-04-24 00:13:35 -0300

    release version

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const textArea = document.querySelector(".text-area");
 const mensagem = document.querySelector(".mensagem");
 
+
 function btnEncriptar() {
     const textoEncriptado = encriptar(textArea.value);
     mensagem.value = textoEncriptado;
@@ -11,14 +12,13 @@ function encriptar(stringEncriptada) {
 
     let matrizCodigo = [["e" , "enter"] , ["i" , "imes"] , ["a" , "ai"] , ["o" , "ober"] , ["u" , "ufat"]];
     stringEncriptada = stringEncriptada.toLowerCase();
-
+    
     for(let i = 0; i < matrizCodigo.length; i++) {
         if (stringEncriptada.includes(matrizCodigo[i][0])) {
             stringEncriptada = stringEncriptada.replaceAll(matrizCodigo[i][0], matrizCodigo[i][1]);
         }
     }
-
-    return stringEncriptada
+    return stringEncriptada.replace(/[âãàáåäêèéëîìíïôõòóöûùúüñýç]/g, "");
 }
 
 function btnDesencriptar() {
